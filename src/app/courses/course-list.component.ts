a76c8e75bda1e55b646c9b643284bc52f4945335
Wesley <wesleysb07@gmail.com>
2021-05-30 22:21:59 -0300

    First Commit of Course

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit {
+    
+    private courses: Array<Course>;
+    private filterBy: string;
+    
+    public filteredCourses: Array<Course>;
+
+    constructor(private courseService: CourseService) { }
+    
+    ngOnInit(): void {
+        this.courses = this.courseService.retrieveAll();
+        this.filteredCourses = this.courses;
+    }
+    
+
+    set filter(value: string) {
+        this.filterBy = value;
+        this.filteredCourses = this.courses.filter((course: Course) => course.name.toLowerCase().indexOf(this.filterBy.toLowerCase()) > -1 );
+    }
+
+    get filter() {
+        return this.filterBy;
+    }
+
+}
